082b94e19e681e3c43f41bd4bb8a8227fd13fb77
Julliann Dulay <jullianndulay@gmail.com>
2020-10-19 19:40:03 -0700

    removed footer for now

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './Header';
-import Footer from './Footer';
 import Products from './Products';
 import TsaaNa from '../Components/Restaurants/TsaaNa'
 import CasaDapitan from '../Components/Restaurants/CasaDapitan'
